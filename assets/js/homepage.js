e08e44760df303fa97a8db017b09d3ad9fa87135
Mark Dale <msdaledad@gmail.com>
2021-11-17 12:04:10 -0500

    fetch not returns formatted data

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,5 +1,12 @@
 var getUserRepos = function() {
-  fetch("https://api.github.com/users/octocat/repos");
+  var response = 
+    fetch("https://api.github.com/users/octocat/repos")
+      .then(function(response) {
+        response.json()
+          .then(function(data) {
+            console.log(data);
+          });
+      });
 };
 
 getUserRepos();
